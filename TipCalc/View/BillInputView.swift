8c8dfc9e84f402ba7bbc30fd5d9d9daddf6de673
anmolkalra17 <anmolkalra17@gmail.com>
2023-11-02 19:44:20 +0530

    Create and add logo view

---
+++ TipCalc/View/BillInputView.swift
@@ -0,0 +1,24 @@
+//
+//  BillInputView.swift
+//  TipCalc
+//
+//  Created by Anmol Kalra on 02/11/23.
+//
+
+import Foundation
+import UIKit
+
+class BillInputView: UIView {
+	init() {
+		super.init(frame: .zero)
+		layout()
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	private func layout() {
+		backgroundColor = .green
+	}
+}
---
+++ TipCalc/View/ResultView.swift
@@ -0,0 +1,24 @@
+//
+//  ResultView.swift
+//  TipCalc
+//
+//  Created by Anmol Kalra on 02/11/23.
+//
+
+import Foundation
+import UIKit
+
+class ResultView: UIView {
+	init() {
+		super.init(frame: .zero)
+		layout()
+	}
+	
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+	
+	private func layout() {
+		backgroundColor = .blue
+	}
+}
