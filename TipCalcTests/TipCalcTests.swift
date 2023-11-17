66aed9235fd002aef6bebcf4755408ba76f70ba2
anmolkalra17 <anmolkalra17@gmail.com>
2023-11-17 20:02:56 +0530

    Complete unit tests

--- TipCalcTests/TipCalcTests.swift
+++ TipCalcTests/TipCalcTests.swift
@@ -15,11 +15,15 @@ final class TipCalcTests: XCTestCase {
 	private var sut: CalculatorVM!
 	private var cancellables: Set<AnyCancellable>!
 	
-	private let logoViewTapSubject = PassthroughSubject<Void, Never>()
+	private var logoViewTapSubject: PassthroughSubject<Void, Never>!
+	private var audioPlayerService: MockAudioPlayerService!
 	
 	override func setUp() {
 		super.setUp()
-		sut = .init()
+		
+		audioPlayerService = .init()
+		sut = .init(audioPlayerService: audioPlayerService)
+		logoViewTapSubject = .init()
 		cancellables = .init()
 	}
 	
@@ -27,6 +31,8 @@ final class TipCalcTests: XCTestCase {
 		super.tearDown()
 		sut = nil
 		cancellables = nil
+		audioPlayerService = nil
+		logoViewTapSubject = nil
 	}
 	
 	func testResultWithoutTipFor1Person() {
@@ -89,10 +95,35 @@ final class TipCalcTests: XCTestCase {
 		}.store(in: &cancellables)
 	}
 	
+	func testSoundPlayedAndCalculatorResetOnLogoViewTap() {
+		//	given
+		let input = buildInput(bill: 100, tip: .tenPercent, split: 2)
+		let output = sut.transform(input: input)
+		let expectation1 = XCTestExpectation(description: "Reset calculator called")
+		let expectation2 = audioPlayerService.expectation
+		//	Since code is async, 'then' gets fired before 'when'
+		//	then
+		output.resetCalculatorPublisher.sink { _ in
+			expectation1.fulfill()
+		}.store(in: &cancellables)
+		//	when
+		logoViewTapSubject.send()
+		wait(for: [expectation1, expectation2], timeout: 1.0)
+	}
+	
 	private func buildInput(bill: Double, tip: Tip, split: Int) -> CalculatorVM.Input {
 		return .init(billPublisher: Just(bill).eraseToAnyPublisher(),
 					 tipPublisher: Just(tip).eraseToAnyPublisher(),
 					 splitPublisher: Just(split).eraseToAnyPublisher(),
 					 logoViewTapPublisher: logoViewTapSubject.eraseToAnyPublisher())
+	}
+}
+
+class MockAudioPlayerService: AudioPlayerService {
+	
+	var expectation = XCTestExpectation(description: "playSound is called")
+	
+	func playSound() {
+		expectation.fulfill()
 	}
 }
